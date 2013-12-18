1d464c655559cebc522285873ec434923b0b27ec
Oleg Mytnyk <oleg.mytnyk@materialise.kiev.ua>
2013-12-18 10:25:32 +0200

    Handling exit

--- GarretTheShadow/src/ua/kiev/mytnyk/garrettheshadow/Run.java
+++ GarretTheShadow/src/ua/kiev/mytnyk/garrettheshadow/Run.java
@@ -2,6 +2,8 @@ package ua.kiev.mytnyk.garrettheshadow;
 
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.app.Activity;
 import android.content.res.Configuration;
 
@@ -28,7 +30,28 @@ public class Run extends Activity {
 	public void onConfigurationChanged(Configuration newConfig)
 	{
 	    super.onConfigurationChanged(newConfig);
-
 	}
 
+	@Override
+	public void onBackPressed() {
+		return; // do not exit on pressing back
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.exit:
+	        	finish();
+	            return true;
+	        default:
+	            return super.onContextItemSelected(item);
+	    }
+	}
 }
